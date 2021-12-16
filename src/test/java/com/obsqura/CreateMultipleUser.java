24cd8a80904d3f75dd9a43fe21ee2093ded25f9b
sreekanth <sreekanthptvnr@gmail.com>
2021-12-16 23:04:47 +0530

    Added data provider t o read data

--- src/test/java/com/obsqura/CreateMultipleUser.java
+++ src/test/java/com/obsqura/CreateMultipleUser.java
@@ -13,11 +13,11 @@ import api.F1_request;
 
 public class CreateMultipleUser {
 	
-	@Test
-	public void test1() {
-		HashMap<String,String>apidata= new HashMap<String,String>();
-		apidata.put("name", "ABC");
-		apidata.put("job", "doctor");
+	 @Test (dataProvider = "data-provider")
+	public void test1(String a,String b) {
+		HashMap<String,String>apiData= new HashMap<String,String>();
+		apiData.put("name", "a");
+		apiData.put("job", "b");
 	    F1_request req = new F1_request("https://reqres.in","Sample.json",apidata);
 	    F1_Response resp =ApiHelper.hitAPI(req);
 		String rspBody =resp.getResponsebody();
@@ -26,7 +26,13 @@ public class CreateMultipleUser {
 		System.out.println(respCode);
 		Assert.assertEquals(respCode, 201);
 	}
-	
+	@DataProvider (name = "data-provider")
+
+	public  Object[][] dp(){
+		
+		return  new Object[][] {{"Sreekanth","engineer"}, {"Rahul","doctor"}};
+	}
+
 	
 	}
 
